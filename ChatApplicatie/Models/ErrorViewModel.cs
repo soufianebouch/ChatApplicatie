b6f16ecdff9622db5fc6e57737a035f1f2be0e58
Student Education <Student@rdeducation.be>
2019-09-20 15:42:58 +0200

    InitialCommit

---
+++ ChatApplicatie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChatApplicatie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
